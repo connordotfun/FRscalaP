f3d1d9f0906fcb27f9ed1b2d51f4c7044993b292
Ethan Hunter <fc.spaceman@gmail.com>
2019-09-04 19:22:12 -0600

    fix build.gradle. Being prototyping API.

--- src/main/scala/FRscalaP/Main.scala
+++ src/main/scala/FRscalaP/Main.scala
@@ -1,9 +1,17 @@
 package FRscalaP
 
-object Main extends App {
-  def foo(x: Int, y: Int) = print(x + " and " + y)
-  def fooTupled = (foo _).tupled
-  val tuple = (10, 10)
 
-  fooTupled(tuple)
+object Main extends App {
+
+
+  val input = Signal.input[Int]()
+
+  val b = Signal.map[Int, Float](input, (x: Int) => x.toFloat + 0.1f)
+  val c = Signal.map(input, (x: Int) => x.toString)
+
+  val d: Signal[(Float, String)] = Signal.merge(b, c)
+
+  val e = Signal.map(d, ((x: Float, y: String) => x + " is 0.1 higher than " + y).tupled)
+
+  val graph = Signal.output(e)
 }
--- src/main/scala/FRscalaP/Signal.scala
+++ src/main/scala/FRscalaP/Signal.scala
@@ -3,3 +3,18 @@ package FRscalaP
 class Signal[T]() {
 
 }
+
+class Graph() {
+
+}
+
+object Signal {
+
+  def input[T](): Signal[T] = new Signal[T]
+
+  def map[T, V](signal: Signal[T], func: T => V): Signal[V] = ???
+
+  def merge[T, V](first: Signal[T], second: Signal[V]): Signal[(T, V)] = ???
+
+  def output[T](signal: Signal[T]): Graph = ???
+}
